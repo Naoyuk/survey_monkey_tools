04831272c76bac30d7bfbdcc4bf8d7cacd912725
Naoyuki Ishida <39.ishida@gmail.com>
2023-08-28 20:09:05 -0700

    start develop gem

---
+++ lib/survey_monkey_tools.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "survey_monkey_tools/version"
+
+module SurveyMonkeyTools
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/survey_monkey_tools_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe SurveyMonkeyTools do
+  it "has a version number" do
+    expect(SurveyMonkeyTools::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
